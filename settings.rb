a2d117b83b8329e004b435f75cb51ed504986a3d
Jamison Dance <jergason@gmail.com>
2011-01-27 10:52:42 -0700

    removing other environments from production settings.rb

--- settings.rb
+++ settings.rb
@@ -1,14 +1,5 @@
 #Settings for application
 require 'dm-core'
-
-configure :test do
-  set :db_path, "sqlite://#{File.join(File.dirname(__FILE__), 'db', 'test.db')}"
-end
-
-configure :development do
-  set :db_path, "sqlite://#{File.join(File.dirname(__FILE__), 'db', 'development.db')}"
-end
-
 set :environment, :production
 DataMapper::Logger.new($stdout, :default)
 DataMapper.setup(:default, ENV['DATABASE_URL'] || settings.db_path)

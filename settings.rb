8e5c596d73c1b93590d4754bc484cd41dbbfaea1
Jamison Dance <jergason@gmail.com>
2011-01-27 11:31:56 -0700

    add code reloading

--- settings.rb
+++ settings.rb
@@ -5,8 +5,11 @@ configure :test do
   set :db_path, "sqlite://#{File.join(File.dirname(__FILE__), 'db', 'test.db')}"
 end
 
-configure :development do
+configure :development do |config|
   set :db_path, "sqlite://#{File.join(File.dirname(__FILE__), 'db', 'development.db')}"
+  config.also_reload "lib/*.rb"
+  config.also_reload "lib/truck_pricer/*.rb"
+  config.also_reload "lib/truck_pricer/models/*.rb"
 end
 
 set :environment, :development

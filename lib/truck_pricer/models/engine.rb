964dff7cb707b37643d9703aa6f07b2bcc9e24d3
Jamison Dance <jergason@gmail.com>
2011-02-02 12:57:11 -0700

    bug fix, also remove whitespace from end of parameters

--- lib/truck_pricer/models/engine.rb
+++ lib/truck_pricer/models/engine.rb
@@ -1,7 +1,7 @@
 module TruckPricer
   class Engine
     include DataMapper::Resource
-    VIN_INDEX = 4
+    VIN_INDEX = 6
     property :id, Serial, :key => true
     property :name, String, :required => true
     property :vin_string, String
